f47aa3fd1f5dd937fc18119544681aa591786eaf
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-05-03 19:17:25 +1000

    writing get and post methods for notes, adjusting some file structure

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const uuid = require('./helpers/uuid')
-//const api = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,7 +10,6 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -25,9 +23,42 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, notes) =>{
+        if(err){
+            console.log("Error reading data");
+        }
+        else{
+            return res.json(JSON.parse(notes));
+        }
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+  if (req.body){
+    const note = {
+      title,
+      text,
+      id: uuid()
+    }
+    fs.readFile('./db/db.json', (err, data) => {
+      if(err){
+        console.log(err);
+      }
+      else{
+        const notes = JSON.parse(data);
+        notes.push(note);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+          if(err){
+            console.log(err);
+          }
+        });
+      }
+    });
+    return res.json("Success");
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
