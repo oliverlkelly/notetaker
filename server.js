c9ce3501e28ef77a7f5273d7710ae63c9ce102a0
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-05-03 20:41:35 +1000

    added wildcard route

--- server.js
+++ server.js
@@ -86,6 +86,10 @@ app.delete('/api/notes/:id', (req, res) => {
   })
 });
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
